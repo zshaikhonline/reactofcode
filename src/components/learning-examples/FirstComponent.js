1aad77980002093b4cdac987f41a22e61526b8d4
ZeeshanShaikh <zeeshanshaikhonline@gmail.com>
2020-08-24 04:18:39 +0530

    adding functional components

---
+++ src/components/learning-examples/FirstComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+//class component
+class FirstComponent extends Component {
+    render() {
+        return (
+            <div className="firstComponent">
+                using class component
+            </div>
+        )
+    }
+}
+
+
+export default FirstComponent
---
+++ src/components/learning-examples/SecondComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//function component
+function SecondComponent() {
+    return (
+        <div className="secondComponent">
+            using function component
+        </div>
+    )
+}
+
+export default SecondComponent
